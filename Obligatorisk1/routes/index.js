6f21438caf4af40c50be694ae399fe491ba9debd
Peter Ring <pede_ring@hotmail.com>
2016-11-09 17:47:36 +0100

    mongoDb

--- Obligatorisk1/routes/index.js
+++ Obligatorisk1/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -9,12 +15,97 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Workouts' });
 });
 
+/* Put new Workout in database */
 router.post('/add', function(req, res, next)
 {
 
-console.log("TESTING");
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
 
-res.render('index', { title: 'Workouts' });
+//Inserting into documents
+insertDocuments(db, function(){
+   db.close();
+}); 
 });
+
+
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+findDocuments(db, function(){
+   db.close();
+});
+});
+
+
+
+ res.render('index', { title: 'Workouts' });
+
+});
+
+/* Insert function */ 
+var insertDocuments = function(db, callback){
+
+console.log("Posting to database");
+
+//Getting table of exercises
+ var table = res.document.getElementById("newWorkoutTable");
+ var rows = table.rows.length;
+
+        for (var r = 0, rows; r < n; r++) {
+          var exercise = new Object();
+          exercise.name = table.rows[r].cell[0].innerHTML;
+          exercise.Description = table.rows[r].cell[1].innerHTML;
+          exercise.Sets = table.rows[r].cell[2].innerHTML;
+          exercise.Repetitions = table.rows[r].cell[3].innerHTML;
+          
+          db.collection('Workouts').insertOne(exercise.toJSON(),  function(err, result)
+{
+  assert.equal(err, null);
+  console.log("Inserted the following records");
+  callback();
+});
+
+
+        }
+
+
+
+}
+
+
+/*Read Function */
+var findDocuments = function(db, callback) {
+// Get the documents collection
+var collection = db.collection('Workouts').find( );
+// Find all documents
+collection.each(function(err, docs) {
+assert.equal(err, null);
+if(docs != null)
+{
+  console.log("Found the following records");
+  console.dir(docs);
+}
+else
+{
+  console.log("No documents in database");
+  callback();
+}
+});
+}
+
+var removeDocuments = function(db, callback) {
+// Get the documents collection
+var collection = db.collection('Workouts').deleteMany(
+      { "test" : "Hello"}, function(err, results){
+        callback();
+      }
+)
+
+
+}
 
 module.exports = router;
