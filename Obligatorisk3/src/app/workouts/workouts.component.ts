41e1522968d619f3ea7a5e765ba59193bed1a87f
Peter Ring <pede_ring@hotmail.com>
2016-11-21 14:34:43 +0100

    WorkoutComponent

--- Obligatorisk3/src/app/workouts/workouts.component.ts
+++ Obligatorisk3/src/app/workouts/workouts.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Workout } from './classes/workout';
+import { WorkoutService } from './services/workout.service';
+
 
 @Component({
-  selector: 'app-workouts',
-  templateUrl: './workouts.component.html',
-  styleUrls: ['./workouts.component.css']
+    moduleId: module.id,
+    selector: 'my-heroes',
+    templateUrl: 'heroes.component.html',
+    styleUrls: [`heroes.component.css`],
+
 })
-export class WorkoutsComponent implements OnInit {
 
-  constructor() { }
+export class WorkoutComponent implements OnInit {
+   
+    title = 'Your workouts';
+    selectedWorkout: Workout;
+    workouts: Workout[];
 
-  ngOnInit() {
-  }
+    constructor(
+        private router: Router,
+        private heroService: WorkoutService) { }
 
+
+    onSelect(workout: Workout): void {
+        this.selectedWorkout = workout;
+    };
+    getHeroes(): void {
+        this.heroService.getHeroes().then(heroes => this.workouts = heroes);
+    };
+    ngOnInit(): void {
+        this.getHeroes();
+    };
+
+    gotoDetail(): void {
+        this.router.navigate(['/detail', this.selectedWorkout.id]);
+    }
+
+    add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        this.heroService.create(name)
+            .then(hero => {
+            this.workouts.push(hero);
+            this.selectedWorkout = null;
+            });
+    }
+    delete(workout: Workout): void {
+        this.heroService
+            .delete(workout.id)
+            .then(() => {
+                this.workouts = this.workouts.filter(h => h !== workout);
+                if (this.selectedWorkout === workout) { this.selectedWorkout = null; }
+            });
+    }       
 }
