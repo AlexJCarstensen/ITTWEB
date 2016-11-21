224adac21252395169cc576832bc555f6ed019e7
Peter Ring <pede_ring@hotmail.com>
2016-11-21 14:23:42 +0100

    Generated alot og stuff

--- Obligatorisk3/src/app/app.module.ts
+++ Obligatorisk3/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WorkoutsComponent } from './workouts/workouts.component';
+import { WorkoutDetailComponent } from './workout-detail/workout-detail.component';
+import { ExercisesComponent } from './exercises/exercises.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    WorkoutsComponent,
+    WorkoutDetailComponent,
+    ExercisesComponent
   ],
   imports: [
     BrowserModule,
